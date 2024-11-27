9854d8b76c1fe1d13b798ad7fd0e3bb40fbc659f
Aditya kumar <144882804+aditya6296@users.noreply.github.com>
2024-11-27 13:34:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Hello js");
+
+let size = prompt("Enter the size of number");
+
+// document.write(`<h1> Number of Users ${size}</h1>`);
+
+let array = [];
+
+for (let i = 0; i < size; i++) {
+  let name =prompt("What's your name ?");
+  array.push(name);
+}
+
+for(let i = 0; i < size; i++){
+  document.write(<li>array[i]</li>);
+  document.write("<br>");
+}
+
